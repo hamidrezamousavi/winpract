fcf13db983c902b56df62658e41c94d087dd6350
test <test@test.com>
2021-05-19 15:17:27 +0430

    excel

--- test.py
+++ test.py
@@ -6,9 +6,14 @@ filepath = path+'\\t.xlsx'
 
 xlApp = Dispatch("Excel.Application")
 xlApp.Visible = 1
-xlApp.Workbooks.Add(filepath)
+xlApp.Workbooks.Open(filepath)
 if xlApp.Workbooks('t1').Sheets('Sheet2').Cells(1,1).Value == 0:
-    xlApp.Workbooks('t1').Sheets('Sheet1').Cells(1,1).Interior.Color = 255
+    xlApp.Workbooks('t1').Sheets('Sheet1').Cells(1,1).Interior.Color = 150
+
+sheet = xlApp.Workbooks('t1').Sheets(1)
+sheet.Cells(2,2).Formula = '=A2*2'
 
 
+xlApp.Workbooks(1).SaveAs(Filename = filepath)
+xlApp.Workbooks(1).Close()
 
